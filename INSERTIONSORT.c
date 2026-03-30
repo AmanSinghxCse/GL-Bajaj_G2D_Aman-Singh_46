981f6d6a0163cf0a5ce52fd45309f8ec635fed73
AmanSinghxCse <amansingh919822@gmail.com>
2026-03-31 00:11:39 +0530

    Add files via upload

---
+++ INSERTIONSORT.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void insertionsort(int arr[],int n){
+    int i,key,j;
+    for(i=1;i<n;i++){
+        key = arr[i];
+        j = i-1;
+        while (j>=0 && arr[j] > key){
+            arr[j+1] =  arr[j];
+            j = j-1;
+        }
+        arr[j+1]=key;
+    }
+}
+void printarray(int arr[],int n){
+    for(int i=0; i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    printf(" \n ");
+}
+int main(){
+    int arr[]= {12,11,13,5,6};
+    int n=sizeof(arr) / sizeof(arr[0]);
+    printf("ORIGINAL ARRAY : \n");
+    printarray(arr,n);
+    insertionsort(arr,n);
+    printf("SORTED ARRAY IN ASCENDING ORDER IS : \n");
+    printarray(arr,n);
+    
+    return 0;
+}
